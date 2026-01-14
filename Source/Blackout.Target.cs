a9f30c41b4944dbef3815432ecc6204edc5aefbb
Douglas <douglasmcarmona@gmail.com>
2026-01-14 10:40:04 -0300

    versioning update

---
+++ Source/Blackout.Target.cs
@@ -0,0 +1,15 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class BlackoutTarget : TargetRules
+{
+	public BlackoutTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		ExtraModuleNames.Add("Blackout");
+	}
+}
---
+++ Source/BlackoutEditor.Target.cs
@@ -0,0 +1,15 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class BlackoutEditorTarget : TargetRules
+{
+	public BlackoutEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		ExtraModuleNames.Add("Blackout");
+	}
+}

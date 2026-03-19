d779f5c22b42a156dacc6df8b642f0057530fcb7
douglas <douglasmcarrmona@gmail.com>
2026-03-19 18:55:19 -0300

    Documenting BlackoutFunctionLibrary

--- Source/Blackout/Public/Util/BlackoutFunctionLibrary.h
+++ Source/Blackout/Public/Util/BlackoutFunctionLibrary.h
@@ -7,8 +7,9 @@
 #include "BlackoutFunctionLibrary.generated.h"
 
 class UPaperNoteInfo;
+
 /**
- * 
+ * Our custom blueprint function library that can be called from anywhere in the project to perform arbitrary functionalities
  */
 UCLASS()
 class BLACKOUT_API UBlackoutFunctionLibrary : public UBlueprintFunctionLibrary
@@ -16,6 +17,11 @@ class BLACKOUT_API UBlackoutFunctionLibrary : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 	
 public:
+	/**
+	 * Retrieves the PaperNoteInfo data asset from its source (the GameMode)
+	 * @param WorldContextObject An existing object in the world to provide context for this function
+	 * @return The PaperNoteInfo object
+	 */
 	UFUNCTION(BlueprintPure, Category="Blackout|Data", meta = (DefaultToSelf = "WorldContextObject"))
 	static UPaperNoteInfo* GetPaperNoteInfo(const UObject* WorldContextObject);	
 };

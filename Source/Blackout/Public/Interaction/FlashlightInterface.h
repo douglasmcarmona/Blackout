8336d622b849e72d331b48e9c44c90c1811be0da
Douglas <douglasmcarmona@gmail.com>
2026-07-21 12:16:24 -0300

    adjusting light source actors

--- Source/Blackout/Public/Interaction/FlashlightInterface.h
+++ Source/Blackout/Public/Interaction/FlashlightInterface.h
@@ -26,4 +26,12 @@ public:
 	 */
 	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, meta=(ForceAsFunction), Category="Flashlight")
 	void SetBatteryPercentage(const float Percent);
+
+	/**
+	 * Increases the BatteryPercentage propertyu in the implementing class. Allows the battery actor to charge the flashlight's
+	 * battery
+	 * @param Percent The amount of additional charge the flashlight will receive
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, meta=(ForceAsFunction), Category="Flashlight")
+	void AddBatteryPercentage(const float Percent);
 };

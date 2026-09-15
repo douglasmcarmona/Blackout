323d57b8c3964fb0ccb5b9be31cd1cde6bb014e2
Douglas <douglasmcarmona@gmail.com>
2026-09-15 10:58:52 -0300

    Implementing game pause when switching out of game window

--- Source/Blackout/Blackout.Build.cs
+++ Source/Blackout/Blackout.Build.cs
@@ -8,7 +8,7 @@ public class Blackout : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Json", "JsonUtilities" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Json", "JsonUtilities", "Slate" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
